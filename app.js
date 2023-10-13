74614ff5ad1756c3e31395f09dcc0db18047b8c2
Marwan-11 <115376022+Marwan-11@users.noreply.github.com>
2023-10-14 01:25:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+
+  // or
+
+  //   if (sidebar.classList.contains("show-sidebar")) {
+  //     sidebar.classList.remove("show-sidebar");
+  //   } else {
+  //     sidebar.classList.add("show-sidebar");
+  //   }
+});
+
+closeBtn.addEventListener("click", function () {
+  //   sidebar.classList.toggle("show-sidebar");
+
+  //   or
+
+  sidebar.classList.remove("show-sidebar");
+});
